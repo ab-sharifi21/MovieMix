6ca47e97263929c7a303c8471a80c734ec8f44c4
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-05-10 18:35:01 +0200

    Change route-item styles

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+ title: 'Search page',
+ description: 'Search page description',
+};
 
 export default function SearchPage() {
   return (
